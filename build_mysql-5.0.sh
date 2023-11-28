8b9e9f42f5175f8f8258e868407bdf804df63079
ulyszk <39549260+ulyszk@users.noreply.github.com>
2023-11-28 13:49:05 +0100

    Update build_mysql-5.0.sh

--- build_mysql-5.0.sh
+++ build_mysql-5.0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MYSQL_URL="http://dev.mysql.com/get/Downloads/MySQL-5.0/mysql-5.0.96-linux-x86_64-glibc23.tar.gz"
-MYSQL_ASC_URL="http://mysql.he.net/Downloads/MySQL-5.0/mysql-5.0.96-linux-x86_64-glibc23.tar.gz.asc"
+MYSQL_ASC_URL="https://downloads.mysql.com/archives/gpg/?file=mysql-5.0.96-linux-x86_64-glibc23.tar.gz&p=23"
 docker build -t mysql-5.0 -f Dockerfile --build-arg mysql_url=${MYSQL_URL} --build-arg mysql_asc_url=${MYSQL_ASC_URL} .
